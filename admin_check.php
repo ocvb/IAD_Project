e93f2089c7838bf6cafab2d10ea4369d8389727c
ocvb <electrokai1@gmail.com>
2022-01-30 17:57:00 +0800

    remove unwanted code

--- admin_check.php
+++ admin_check.php
@@ -1,33 +1,27 @@
 <?php
 include_once "db.php";
 
-$sql = "SELECT id, email, administrator FROM members";
+$sql = "SELECT ID, email, administrator FROM members";
 $result = mysqli_query($db, $sql);
-$row = mysqli_fetch_array($result);
-echo "hiii";
 
-if ($row['email'] == $_COOKIE['user'] && $row['id'] == 2) {
-   setcookie("adStatus", 'yes', null, "/");
-} else {
-   setcookie("adStatus", 'no', null, "/");
+while ($row = mysqli_fetch_assoc($result) > 0) {
+   if ($row['email'] == $_COOKIE['user']) {
+      setcookie("adStatus", 'yes', null, "/");
+   } else {
+      setcookie("adStatus", 'yes', null, "/");
+   }
+
 }
 ?>
 
-<head>
+<!--<head>
    <script src="js/jquery-3.6.0.min.js" type="application/javascript"></script>
    <script type="module">
       import { getCookie } from "./js/cookies.js";
 
       $(document).ready(function() {
          if (getCookie("user") == 'admin%40admin.com') {
-            <?php
-            setcookie("adStatus", 'yes', null, "/");
-            ?>
-         } else {
-            <?php
-            setcookie("adStatus", 'no', null, "/");   
-            ?>
          }
       });
       </script>
-</head>
+</head>-->
