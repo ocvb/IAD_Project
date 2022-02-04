e52806a7c6daa4ca54bd15896b77fcb3985fd7eb
ocvb <electrokai1@gmail.com>
2022-02-05 00:26:21 +0800

    Fixed AGAIN

--- admin_check.php
+++ admin_check.php
@@ -5,18 +5,20 @@
 <?php
 include_once "db.php";
 
-$sql = "SELECT email, administrator FROM members";
-$result = mysqli_query($db, $sql);
-while ($row = mysqli_fetch_array($result)) {
-   echo $row['email'];
-   if ($row['email'] == $_COOKIE['user']) {
-      setcookie("adStatus", 'yes', null, "/");
-      return true;
-   } else {
-      setcookie("adStatus", 'no', null, "/");
-   }
+function getID() {
+   global $db;
+   $email = $_COOKIE['user'];
+   $sql = "SELECT id FROM members WHERE email = '$email'";
+   $getID = mysqli_query($db, $sql);
+   $row = mysqli_fetch_array($getID);
+   return $row['id'];
+}
 
-   if ($row['email'] == $_COOKIE['user']) {
+$sql = "SELECT id, email, administrator FROM members";
+$result = mysqli_query($db, $sql);
+$id = getID();
+while ($row = mysqli_fetch_array($result)) {
+   if ($row['email'] == $_COOKIE['user'] && $row['administrator'] == 'yes' && $row['id'] == $id) {
       setcookie("adStatus", 'yes', null, "/");
       return true;
    } else {
