0cc96a023f92b32479edeb7060ffa11deaa9dc57
ocvb <electrokai1@gmail.com>
2022-02-05 00:12:23 +0800

    update

--- register.php
+++ register.php
@@ -38,18 +38,22 @@ if (isset($_POST['submit'])) {
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+   <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
    <link rel="stylesheet" href="css/style.css" type="text/css">
    <link rel="stylesheet" href="css/register.css" type="text/css">
-   <link rel="stylesheet" href="css/bootstrap.css" type="text/css">
    <title>Document</title>
 </head>
 
 <body>
-   <nav class="d-flex flex-row justify-content-center">
-      <div class="p-2"><a href="index.html">Home</a></div>
-      <div class="p-2"><a href="shop.php">Shop</a></div>
-      <div class="p-2">Register</div>
-      <div class="p-2"><a href="login.php">Login</a></div>
+   <nav class="navbar navbar-light justify-content-center">
+      <div class="navaddpage">
+         <a class="nav-item nav-link active" href="index.html">Home</a>
+         <a class="nav-item nav-link" href="shop.php">Shop</a>
+         <a class="nav-item nav-link" href="register.php">Register</a>
+         <a class="nav-item nav-link" id="login" href="login.php">Login</a>
+      </div>
    </nav>
 
    <div class="main-container navbar justify-content-center">
