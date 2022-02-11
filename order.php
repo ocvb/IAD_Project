165e01267fac2e7624dd2912a9ea8a6c1ff4881f
ocvb <electrokai1@gmail.com>
2022-02-11 23:16:12 +0800

    design change

--- order.php
+++ order.php
@@ -32,8 +32,6 @@ function product_name($id)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css" type="text/css">
     <link rel="stylesheet" href="css/shop.css" type="text/css">
     <script type="module">
@@ -48,7 +46,7 @@ function product_name($id)
                     document.querySelector("#login").href = "javascript:logout();";
                     document.querySelector("#login").id = "logout";
                     if (getCookie("adStatus") == 'yes') {
-                        $('.navaddpage').append('<a class="nav-item nav-link" href="admin.php">Admin</a>');
+                        $('.navaddpage').append('<li class="nav-item"><a class="nav-link active" href="admin.php">Admin</a></li>');
                     }
                 }
             }
@@ -63,12 +61,12 @@ function product_name($id)
 
 <body>
     <!--TODO: Header, Nav, Article, Aside-->
-    <nav class="navbar navbar-light justify-content-center">
-        <div class="navaddpage">
-            <a class="nav-item nav-link active" href="index.html">Home</a>
-            <a class="nav-item nav-link" href="order.php">Order</a>
-            <a class="nav-item nav-link" id="login" href="login.php">Login</a>
-        </div>
+    <nav>
+        <ul class="nav d-flex justify-content-center fixed-top navaddpage">
+            <li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="order.php">Order</a></li>
+            <li class="nav-item"><a id="login" class="nav-link" href="login.php">Login</a></li>
+        </ul>
     </nav>
     <header class="bg-dark py-5">
         <div class="container px-4 px-lg-5 my-5">
@@ -85,7 +83,7 @@ function product_name($id)
                 <div class="col mb-5">
                     <div class="card h-100">
                         <!-- Product image-->
-                        <img class="card-img-top" src="images/bison-burgers.jpg" alt="..." />
+                        <img class="card-img-top" src="images/classic-beef-burger.jpg" alt="..." />
                         <!-- Product details-->
                         <div class="card-body p-4">
                             <div class="text-center">
@@ -143,7 +141,7 @@ function product_name($id)
                         <!-- Sale badge-->
                         <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
                         <!-- Product image-->
-                        <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                        <img class="card-img-top" src="images/classic-beef-burger.jpg" alt="..." />
                         <!-- Product details-->
                         <div class="card-body p-4">
                             <div class="text-center">
@@ -301,6 +299,10 @@ function product_name($id)
             <p class="m-0 text-center text-white">Copyright &copy; BurgurPro 2022</p>
         </div>
     </footer>
+
+    <!-- javascript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </body>
 
 </html>
