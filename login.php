da4e035b8e45dddf65d2c3162645c31c2e5406e1
ocvb <electrokai1@gmail.com>
2022-02-11 23:15:38 +0800

    design updated

--- login.php
+++ login.php
@@ -27,31 +27,30 @@ if (isset($_POST['submit'])) {
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-   <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
    <link rel="stylesheet" href="css/style.css" type="text/css">
+   <link rel="stylesheet" href="css/login.css" type="text/css">
    <title>Login</title>
 </head>
 
-<body>
-   <nav class="navbar navbar-light justify-content-center">
-      <div class="navaddpage">
-         <a class="nav-item nav-link active" href="index.html">Home</a>
-         <a class="nav-item nav-link" href="order.php">Order</a>
-         <a class="nav-item nav-link" id="login" href="login.php">Login</a>
-      </div>
+<body class="overflow-hidden">
+   <nav>
+      <ul class="nav d-flex justify-content-center fixed-top navaddpage">
+         <li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>
+         <li class="nav-item"><a class="nav-link" href="order.php">Order</a></li>
+         <li class="nav-item"><a id="login" class="nav-link" href="login.php">Login</a></li>
+      </ul>
    </nav>
-
-   <div class="main-container navbar justify-content-center">
-      <div class="form-container row g-4">
+   
+   <div class="container main-container justify-content-center">
+      <div class="row form-container g-4">
          <h3 class="h3 text-center">Login Panel</h3>
-         <div class="form-form">
+         <div class="form">
             <form method="POST" class="col">
                <div class="top-form form-group">
                   <label for="">Email:</label>
                   <input type="email" name="email" class="form-control" placeholder="Email" required>
                </div>
-               <div class="form-group">
+               <div class="top-form form-group">
                   <label for="">Password:</label>
                   <input type="password" name="password" class="form-control" placeholder="Password" required>
                </div>
@@ -84,6 +83,10 @@ if (isset($_POST['submit'])) {
          </div>
       </div>
    </div>
+
+   <!-- Javscript -->
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+   <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </body>
 
 </html>
