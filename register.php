5e5bc29befca13558ad92049212827cc9e48401c
ocvb <electrokai1@gmail.com>
2022-02-11 23:28:53 +0800

    WEIOFNWONWO FIXESS

--- register.php
+++ register.php
@@ -19,7 +19,8 @@ if (isset($_POST['submit'])) {
       $i = 1;
    }
 
-   function timeout($set, $here) {
+   function timeout($set, $here)
+   {
       $i = 0;
       while ($i < $set) {
          sleep(1);
@@ -39,28 +40,26 @@ if (isset($_POST['submit'])) {
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-   <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
    <link rel="stylesheet" href="css/style.css" type="text/css">
    <link rel="stylesheet" href="css/register.css" type="text/css">
    <title>Document</title>
 </head>
 
 <body>
-   <nav class="navbar navbar-light justify-content-center">
-      <div class="navaddpage">
-         <a class="nav-item nav-link active" href="index.html">Home</a>
-         <a class="nav-item nav-link" href="order.php">Order</a>
-         <a class="nav-item nav-link" href="register.php">Register</a>
-         <a class="nav-item nav-link" id="login" href="login.php">Login</a>
-      </div>
+   <nav>
+      <ul class="nav d-flex justify-content-center fixed-top navaddpage">
+         <li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>
+         <li class="nav-item"><a class="nav-link" href="order.php">Order</a></li>
+         <li class="nav-item"><a class="nav-link" href="register.php">Register</a></li>
+         <li class="nav-item"><a id="login" class="nav-link" href="login.php">Login</a></li>
+      </ul>
    </nav>
 
-   <div class="main-container navbar justify-content-center">
-      <div class="form-container row g-4">
+   <div class="container main-container justify-content-center">
+      <div class="row form-container g-4">
          <h3 class="h3 text-center">Register Panel</h3>
          <form method="POST" class="col">
-            <div class="form-group">
+            <div class="top-form form-group">
                <label for="">Name:</label>
                <input type="text" name="name" class="form-control">
             </div>
@@ -94,6 +93,10 @@ if (isset($_POST['submit'])) {
          </form>
       </div>
    </div>
+
+   <!-- javscript -->
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+   <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </body>
 
 </html>
