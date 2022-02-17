930de99448d616c3a7260351e421acf907ec1485
ocvb <electrokai1@gmail.com>
2022-02-17 13:46:22 +0800

    e

--- order.php
+++ order.php
@@ -1,6 +1,6 @@
 <?php
 include_once "db.php";
-include_once "check_admin.php";
+include_once "admin_check.php";
 echo "<script>var storage = sessionStorage.getItem('user');</script>";
 $storage = '<script>document.write(storage);</script>';
 
