1cc25513d51ee6a98b848e1b026ee5fce6bf746b
ocvb <electrokai1@gmail.com>
2022-02-24 14:15:37 +0800

    :((

--- account.php
+++ account.php
@@ -6,6 +6,7 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" type="text/css">
    <link rel="stylesheet" href="css/style.css" type="text/css">
@@ -27,17 +28,16 @@
             if (getCookie("user") != "notlogged") {
                document.querySelector("#logout").href = "javascript:logout();";
                if (getCookie("adStatus") == 'yes') {
-                  $("#accountpage li:first").append('<li class="nav-item account-item">- <a href="#">Admin</a></li>');
+                  $("#accountpage li:first").append('<li class="nav-item account-item">- <a id="admin" href="#">Admin</a></li>');
                }
             } else {
                window.location.href = "login.php";
             }
-
+            
             $.post("admin_check.php");
-
+            
          }
          checklogin();
-
          /*if (sessionStorage.getItem('user') == 'notlogged') {
             document.querySelector("#para").textContent = "test test";
          }*/
@@ -58,7 +58,7 @@
    </nav>
 
 
-   <div class="account-tabs container d-flex align-items-center border">
+   <div class="account-tabs container-fluid">
       <div class="container position-relative float-left" data-aos="zoom-in" data-aos-delay="100">
          <ul class="text-white list-unstyled accountpage" id="accountpage">
             <li class="nav-item account-item">- <a href="#">Account Details</a></li>
@@ -67,7 +67,11 @@
          </ul>
       </div>
 
-      <?php include_once "./adminpages/admin.php" ?>
+      <div class="container payload ml-4 align-center text-center"></div>
+
+      <script type="text/javascript">
+         $(".payload").load("./adminpages/admin.php");
+      </script>
    </div>
 
 
@@ -85,6 +89,10 @@
       function logout() {
          $.post("cookies.php");
          window.location.reload();
+      }
+
+      function admin() {
+         $.post("./adminpages/admin.php");
       }
    </script>
 </body>
--- adminpages/admin.php
+++ adminpages/admin.php
@@ -1,15 +1,51 @@
-<div class="container position-absolute border dynamic-table">
+<?php
+include_once "../db.php";
+
+$email = $_COOKIE['user'];
+$query = "SELECT `regid`, `name`, `course`, `email`, `hp_no`, `reg_date` FROM members WHERE email = '$email'";
+$result = mysqli_query($db, $query);
+$row = mysqli_fetch_array($result);
+
+function td($i)
+{
+   return "<td>$i</td>";
+}
+?>
+
+<script>
+   $(document).ready(function () {
+      
+   })
+</script>
+
+<div class="container-fluid dynamic-table justify-content-center">
    <nav class="border">
       <ul class="nav list-unstyled bg-transparent">
-         <li class="nav-item account-item"><a class="nav-link" href="#">View</a></li>
+         <li class="nav-item account-item"><a id='id' class="nav-link" href="#">View</a></li>
          <li class="nav-item account-item"><a class="nav-link" href="#">Delete</a></li>
          <li class="nav-item account-item"><a class="nav-link" href="#">Update</a></li>
       </ul>
    </nav>
 
-   <div class="data float-left border text-white">
-      <div>
-         aiwjfioawuenf
+   <div class="data border text-white">
+      <!--echo "<tr>".td($row['regid']). td($row['name']). td($row['course'])."</tr>";-->
+      <div class="row">
+         <div class="col-sm">
+            <div><?php echo $row['regid']; ?></div>
+         </div>
+         <div class="col-sm">
+            <div><?php echo $row['name']; ?></div>
+         </div>
+         <div class="col-sm">
+            <div><?php echo $row['course']; ?></div>
+         </div>
+         <div class="col-sm">
+            <div><?php echo $row['email']; ?></div>
+         </div>
+         <div class="col-sm">
+            <div><?php echo $row['hp_no']; ?></div>
+         </div>
       </div>
    </div>
 </div>
+
