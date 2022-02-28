0f96df37f58b61a6a6a1977a55831fd02cfb0a7d
ocvb <electrokai1@gmail.com>
2022-03-01 00:12:18 +0800

    upate

--- db.php
+++ db.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(0);
 $db = mysqli_connect('localhost', "root", 'StudentA', "innovate") or die("Connection failed: " . mysqli_connect_error());
 
 //if ($db == TRUE) { echo "Database connected successfully!"; }
